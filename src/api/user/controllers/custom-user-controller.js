2d723b6cadfa5a1c1c276ee19227c5e74ab20a95
Rychkov Iurii <rychkoviurii@hotmail.com>
2024-06-03 09:37:58 +0300

    Add phone validation to custom-user-controller

--- src/api/user/controllers/custom-user-controller.js
+++ src/api/user/controllers/custom-user-controller.js
@@ -11,6 +11,11 @@ module.exports = createCoreController('plugin::users-permissions.user', ({ strap
       return ctx.unauthorized("You can't update this entry");
     }
 
+    if (!/^(\+\d{1,3}\d{8,9}|0\d{8,9})$/.test(phone)) {
+      ctx.badRequest('Invalid phone number format');
+      return;
+    }
+
     try {
       const updatedUser = await strapi.plugin('users-permissions').service('user').edit(user.id, { phone });
       ctx.send({ user: updatedUser });
