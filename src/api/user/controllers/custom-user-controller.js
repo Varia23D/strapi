e2de20bd28688210c2b1648f0aad6bffafbb7756
Rychkov Iurii <rychkoviurii@hotmail.com>
2024-06-13 00:20:42 +0300

    Add comments

--- src/api/user/controllers/custom-user-controller.js
+++ src/api/user/controllers/custom-user-controller.js
@@ -3,20 +3,22 @@
 const { createCoreController } = require('@strapi/strapi').factories;
 
 module.exports = createCoreController('plugin::users-permissions.user', ({ strapi }) => ({
+                                        // Define the updateMe method
   async updateMe(ctx) {
-    const user = ctx.state.user;
-    const { phone } = ctx.request.body;
+    const user = ctx.state.user;        // Get the currently user from the context
+    const { phone } = ctx.request.body; // Extract the phone number from the request body
 
-    if (!user) {
+    if (!user) {                        // Check if the user is authenticated
       return ctx.unauthorized("You can't update this entry");
     }
 
     if (!/^(\+\d{1,3}\d{8,9}|0\d{8,9})$/.test(phone)) {
-      ctx.badRequest('Invalid phone number format');
+      ctx.badRequest('Invalid phone number format');  // Return error if invalid
       return;
     }
 
     try {
+      // Update the user's phone number in the database
       const updatedUser = await strapi.plugin('users-permissions').service('user').edit(user.id, { phone });
       ctx.send({ user: updatedUser });
     } catch (error) {
--- src/middlewares/isAuthenticated.js
+++ src/middlewares/isAuthenticated.js
@@ -3,7 +3,7 @@
 module.exports = (config, { strapi }) => {
   return async (ctx, next) => {
     if (ctx.state.user) {
-      return next();
+      return next();  // Proceed to the next middleware or route handler if the user is authenticated
     } else {
       return ctx.unauthorized("You must be logged in to perform this action");
     }
