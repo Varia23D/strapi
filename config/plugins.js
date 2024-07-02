c8f60c75c0c92897600ee11ab8f58d2d4583e49e
Rychkov Iurii <rychkoviurii@hotmail.com>
2024-07-02 09:48:25 +0300

    Get access token from the keycloak

--- config/plugins.js
+++ config/plugins.js
@@ -1,7 +1,29 @@
-module.exports = {
+module.exports = ({ env }) => ({
 	// ...
 	'users-permissions': {
 	  enabled: true,
-	},
-	// ...
-  };
+    /* 'keycloak': {
+      enabled: true,
+      serverUrl: 'http://localhost:8080/auth',
+      realm: 'strapi-realm',
+      clientId: 'strapi-client',
+      secret: 'your-secret',
+    }, */
+    // ...
+    config: {
+      providers: {
+        keycloak: {
+          issuer: 'http://localhost:8080/realms/strapi-realm',
+          clientId: 'strapi-client',
+          clientSecret: env('KEYCLOAK_CLIENT_SECRET'),
+          scope: ['openid', 'profile', 'email'],
+          authorizationParams: {
+            response_type: 'code',
+            scope: 'openid profile email',
+          },
+          callbackURL: 'http://localhost:1337/api/connect/keycloak/callback',
+        },
+      },
+    },
+  },
+});
