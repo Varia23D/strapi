b27483c5d76f88b4d159359adf5fc04ce11f6098
Plyushevo <Plyushevo@gmail.com>
2024-07-06 14:27:02 +0300

    sqlite3

--- config/database.js
+++ config/database.js
@@ -26,6 +26,16 @@ module.exports = ({ env }) => {
         schema: env('DATABASE_SCHEMA', 'public'),
       },
       pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
+    },
+    sqlite: {
+      connection: {
+        filename: path.join(
+          __dirname,
+          '..',
+          env('DATABASE_FILENAME', '.tmp/data.db')
+        ),
+      },
+      useNullAsDefault: true,
     }
   };
 
