cadb2a479378c37fe9c78d29a9df174327d0a699
Plyushevo <Plyushevo@gmail.com>
2024-07-06 14:55:50 +0300

    sqlite3

--- app/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'f641f1da3f8f80c4183aa945f2ea86cc'),
-    },
-  },
-});
--- app/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '340b7954-fc44-4ace-af21-8e095c546bab'
-};
